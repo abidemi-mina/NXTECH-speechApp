99acc1032db7b879acea57f98c4735d1f3e631ef
abidemi-mina <aminatabidemi212@gmail.com>
2023-08-14 15:20:41 +0100

    'user-recognition'

--- nd.py
+++
@@ -1,9 +0,0 @@
-import numpy
-x=1j
-print(x**2 == -1)
-for i in range(5):
-    # print(i)
-else:
-    print('Done')
-a = [True,False]
-numpy.sum(a)
--- sample.js
+++
@@ -1,8 +0,0 @@
-let x = 6+3+'3'
-console.log(x)
-
-let arr1= [2,4,6]
-let arr2 = [3,5,7]
-arr1[100] = 8
-console.log(arr1.length);
-// console.log(...arr1,...arr2);
